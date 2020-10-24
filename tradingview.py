466a82ffa28ed14149a535e979e2fe5c56ffc994
svlad4560 <svlad4560@gmail.com>
2020-10-24 12:50:42 -0700

    This is me fucking around with for loops.

---
+++ tradingview.py
@@ -0,0 +1,16 @@
+import backtrader
+# tesla =  TA_Handler()
+#
+# handler.set_symbol_as('TSLA')
+#
+# handler.set_exchange_as_crypto_or_stock("NASDAQ")
+#
+# tesla.set_screener_as_stock("america")
+# tesla.set_interval_as(Interval.INTERVAL_1_DAY)
+# print(tesla.get_analysis().summary)
+
+n = {'a':'sebi','b': 'iuli'}
+# print(n['a'])
+
+for item in n.items():
+    print(item)
